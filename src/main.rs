e11ab0a627922aa60c2b4f62287b8851159d2ba6
Joris Rehm <joris.rehm@wakusei.fr>
2017-06-02 08:35:58 +0200

    drag and drop food from catalog to planning

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate gdk;
 extern crate gtk;
 use gtk::prelude::*;
 use std::fmt;
+use std::rc::*;
+use std::cell::*;
 
 enum Group {
     Vegetable,
@@ -89,7 +91,7 @@ enum Quantity {
 }
 
 struct Part {
-    id : u32,
+    id : usize,
     name : String,
     group : Group,
     section : Section,
@@ -113,7 +115,7 @@ fn default_catalog() -> Catalog {
         section : Section::Greengrocer,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -123,7 +125,7 @@ fn default_catalog() -> Catalog {
         section : Section::Greengrocer,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -133,7 +135,7 @@ fn default_catalog() -> Catalog {
         section : Section::Butcher,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -143,7 +145,7 @@ fn default_catalog() -> Catalog {
         section : Section::Other,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -153,7 +155,7 @@ fn default_catalog() -> Catalog {
         section : Section::Other,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -163,7 +165,7 @@ fn default_catalog() -> Catalog {
         section : Section::Dairy,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     let p = Part {
         id : 0,
@@ -173,7 +175,7 @@ fn default_catalog() -> Catalog {
         section : Section::Dairy,
     };
     c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as u32;
+    c.list.last_mut().unwrap().id = c.list.len() as usize;
     
     return c;
 }
@@ -192,90 +194,136 @@ fn main() {
         Inhibit(false)
     });
     
-    let catalog = default_catalog();
-    
-    setup(window.clone(), &catalog);
+    let catalog : Rc<RefCell<Catalog>> = Rc::new(RefCell::new(default_catalog()));
+        
+    setup(window.clone(), catalog.clone());
     
     window.show_all();
     gtk::main();
 }
 
-fn setup(window : gtk::Window, catalog : &Catalog) {
+fn setup(window : gtk::Window, catalog : Rc<RefCell<Catalog>>) {
 
     // division par deux panneaux
     let paned = gtk::Paned::new(gtk::Orientation::Horizontal);
     window.add(&paned);
 
     // partie gauche : le planning
-//     let left = gtk::ListBox::new();
-    let left = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+    let left = gtk::ListBox::new();
     paned.add1(&left);
     left.set_size_request(300,300);
+    show_planning(&left, catalog.clone());
 
     // partie droite : le catalogue d'ingrédients
     let right = gtk::ListBox::new();
     paned.add2(&right);
     right.set_size_request(300,300);
-    show_catalogue(&right, catalog);
+    show_catalogue(&right, catalog.clone());
     
-    // test de drag&drop
-    let to = gtk::Label::new("Drop here");
-    
-    left.pack_start(&to, true, true, 0);
-    
-    let targets = vec![
-        gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
-    ];
-    
-    to.drag_dest_set(gtk::DEST_DEFAULT_ALL, &targets, gdk::ACTION_COPY);
-    to.connect_drag_data_received(|w, _, _, _, s, _, _| {
-        w.set_text(&s.get_text().unwrap());
-    });
-
 }
 
-fn show_catalogue(target : &gtk::ListBox, catalog : &Catalog) {
+fn show_planning(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
+    let days = [
+        "lundi",
+        "mardi",
+        "mercredi",
+        "jeudi",
+        "vendredi",
+        "samedi",
+        "dimanche",
+    ];
+    let lunchs = [
+        "petit-déjeuner",
+        "déjeuner",
+        "gouter",
+        "diner",
+    ];
+    for day in days.iter() {
+        for lunch in lunchs.iter() {
+            show_empty_lunch(target, &format!("{} {}", day, lunch), catalog.clone());
+        }
+    }
+}
+
+fn show_empty_lunch(target : & gtk::ListBox, label : &str, catalog : Rc<RefCell<Catalog>>) {
+    let targets = vec![
+        gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
+    ];
+    
+    // test de drag&drop
+    let to = gtk::Label::new(label);
+    target.add(&to);
+    
+    to.drag_dest_set(gtk::DEST_DEFAULT_ALL, &targets, gdk::ACTION_COPY);
+    
+    let listbox = target.clone();
+    
+    to.connect_drag_data_received(move |w, _, _, _, s, _, _| {
+        let lbr : gtk::ListBoxRow = w
+            .get_parent()
+            .unwrap()
+            .downcast::<gtk::ListBoxRow>()
+            .unwrap();
+        let drop_pos = lbr.get_index();
+        let part_num : usize = s
+            .get_text().unwrap()
+            .parse().unwrap();
+        let part = & catalog.borrow().list[part_num];
+        let line = create_part(part);
+        listbox.insert(&line, drop_pos+1);
+        line.show_all();
+    });
+}
+
+fn create_part(part : & Part) -> gtk::EventBox {
 
     let targets = vec![
         gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
     ];
 
-    for p in &catalog.list {
-        let eventbox = gtk::EventBox::new();
-        target.add(&eventbox);
-    
-        let line = gtk::Grid::new();
-        eventbox.add(&line);
-        line.set_column_spacing(10);
+    let eventbox = gtk::EventBox::new();
 
-        let name = gtk::Label::new(&p.name as &str);
-        line.add(&name);
-        
-        eventbox.drag_source_set( gdk::MODIFIER_MASK, &targets, gdk::ACTION_COPY);
-        let data = p.id.to_string().clone();
-        eventbox.connect_drag_data_get(move |_, _, s, _, _| {
-            s.set_text(&data, data.len() as i32);
-        });
-        
-        match p.quantity {
-            Quantity::Countable(n) => {
-                let spin = gtk::SpinButton::new_with_range(
-                    1.0, 1000.0, 25.0);
-                line.add(&spin);
-                spin.set_value(n as f64);
-            },
-            Quantity::Divisible(n, u) => {
-                let spin = gtk::SpinButton::new_with_range(
-                    1.0, 50.0, 1.0);
-                line.add(&spin);
-                spin.set_value(n as f64);
-                line.add(&create_combo_of_unit(&u));
-            },
-        };
-        
-        line.add(&gtk::Label::new(&p.group.to_string() as &str));
-        
-        let section : String = format!("({})", p.section);
-        line.add(&gtk::Label::new(&section as &str));
+    let line = gtk::Grid::new();
+    eventbox.add(&line);
+    line.set_column_spacing(10);
+
+    let name = gtk::Label::new(&part.name as &str);
+    line.add(&name);
+    
+    eventbox.drag_source_set( gdk::MODIFIER_MASK, &targets, gdk::ACTION_COPY);
+    let data = part.id.to_string().clone();
+    eventbox.connect_drag_data_get(move |_, _, s, _, _| {
+        s.set_text(&data, data.len() as i32);
+    });
+    
+    match part.quantity {
+        Quantity::Countable(n) => {
+            let spin = gtk::SpinButton::new_with_range(
+                1.0, 1000.0, 25.0);
+            line.add(&spin);
+            spin.set_value(n as f64);
+        },
+        Quantity::Divisible(n, u) => {
+            let spin = gtk::SpinButton::new_with_range(
+                1.0, 50.0, 1.0);
+            line.add(&spin);
+            spin.set_value(n as f64);
+            line.add(&create_combo_of_unit(&u));
+        },
+    };
+    
+    line.add(&gtk::Label::new(&part.group.to_string() as &str));
+    
+    let section : String = format!("({})", part.section);
+    line.add(&gtk::Label::new(&section as &str));
+
+    return eventbox;
+}
+
+fn show_catalogue(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
+
+    for p in &catalog.borrow().list {
+        let line = create_part(p);
+        target.add(&line);
     }
 }
