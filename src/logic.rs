f45d60a5a9e15f587b5fac3f60f0bee48488ff92
Joris Rehm <joris.rehm@wakusei.fr>
2017-06-03 08:17:07 +0200

    refactor data management

--- src/logic.rs
+++ src/logic.rs
@@ -1,8 +1,13 @@
 
+#![allow(dead_code)]
+
+
+extern crate rand;
+
 use std::fmt;
+use std::collections::HashMap;
 
-
-struct Data {
+pub struct Data {
     catalog : Catalog,
     book : Book,
     planning : Planning,
@@ -10,9 +15,41 @@ struct Data {
     stock : Stock,
 }
 
-pub struct Catalog {
-    pub list : Vec<Part>,
+impl Data {
+    pub fn new() -> Data {
+        Data {
+            catalog : Catalog {
+                ingredients : HashMap::new(),
+                sections : HashMap::new(),
+                groups : HashMap::new(),
+            },
+            book : Book {},
+            planning : Planning {},
+            history : History {},
+            stock : Stock {},
+        }
+    }
+    
+    pub fn add_section(&mut self, section : &Section) {
+        // TODO check if id is unique
+        self.catalog.sections.insert(section.id, section.clone());
+    }
+    pub fn add_group(&mut self, group : &Group) {
+        // TODO check if id is unique
+        self.catalog.groups.insert(group.id, group.clone());
+    }
+    pub fn add_ingredient(&mut self, ingredient : &Ingredient) {
+        // TODO check if id is unique
+        self.catalog.ingredients.insert(ingredient.id, ingredient.clone());
+    }
 }
+        
+struct Catalog {
+    ingredients : HashMap<Id,Ingredient>,
+    sections : HashMap<Id,Section>,
+    groups : HashMap<Id,Group>,
+}
+
 
 struct Book {
 }
@@ -26,89 +63,87 @@ struct History {
 struct Stock {
 }
 
-// struct Id { u32 };
-// 
-// impl Id {
-//     new() -> Id {
-//         Id(22)
-//     }
-// }
-
-pub struct Part {
-    pub id : usize,
+#[derive(Clone)]
+pub struct Ingredient {
+    pub id : Id,
     pub name : String,
     pub group : Group,
     pub section : Section,
     pub quantity : Quantity,
 }
 
-
-
-pub enum Group {
-    Vegetable,
-    Fruit,
-    Protein,
-    Carbohydrate,
-    Fat,
-    Dairy,
-    Cheese,
-}
-
-impl fmt::Display for Group {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let printable = match *self {
-                Group::Vegetable => "Légume",
-                Group::Fruit => "Fruit",
-                Group::Protein => "Protéine",
-                Group::Carbohydrate => "Féculent",
-                Group::Fat => "Graisse",
-                Group::Dairy => "Laitage",
-                Group::Cheese => "Fromage",
-            };
-        write!(f, "{}", printable)
+impl Ingredient {
+    pub fn new(
+        name : &str,
+        group : &Group,
+        section : &Section,
+        quantity : &Quantity
+    ) -> Ingredient {
+        Ingredient {
+            id : Id::new(),
+            name : name.to_string(),
+            group : group.clone(),
+            section : section.clone(),
+            quantity : quantity.clone(),
+        }
     }
 }
 
-pub enum Section {
-    Other,
-    Greengrocer,
-    Frozen,
-    Dairy,
-    Organic,
-    Drink,
-    Starchy,
-    Condiment,
-    Butcher,
-    Fishshop,
+
+#[derive(Clone)]
+pub struct Group {
+    id : Id,
+    name : String,
+}
+
+impl Group {
+    pub fn new(name : &str) -> Group {
+        Group{
+            id : Id::new(),
+            name : name.to_string(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Section {
+    id : Id,
+    name : String,
+}
+
+impl Section {
+    pub fn new(name : &str) -> Section {
+        Section{
+            id : Id::new(),
+            name : name.to_string(),
+        }
+    }
 }
 
 impl fmt::Display for Section {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let printable = match *self {
-                Section::Other => "Autre",
-                Section::Greengrocer => "Primeur",
-                Section::Frozen => "Surgelé",
-                Section::Dairy => "Crémerie",
-                Section::Organic => "Bio",
-                Section::Drink => "Boisson",
-                Section::Starchy => "Féculents",
-                Section::Condiment => "Condiment",
-                Section::Butcher => "Viande",
-                Section::Fishshop => "Poisson",
-            };
-        write!(f, "{}", printable)
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
     }
 }
 
 #[derive(Clone,Copy)]
+pub struct Quantity {
+    val : f64,
+    unit : Unit,
+}
+
+
+#[derive(Clone,Copy)]
 pub enum Unit {
-    Gram = 0,
+    Portion = 0,
+    Gram,
     Centilitre,
 }
 
 impl fmt::Display for Unit {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             let printable = match *self {
+                Unit::Portion => "u.",
                 Unit::Gram => "g",
                 Unit::Centilitre => "cl",
             };
@@ -116,85 +151,114 @@ impl fmt::Display for Unit {
     }
 }
 
-pub enum Quantity {
-    Divisible(u16, Unit),
-    Countable(u16)
+#[derive(Clone,Copy,PartialEq,Eq,Hash)]
+pub struct Id( u64 );
+
+impl Id {
+    fn new() -> Id {
+        Id(rand::random())
+    }
 }
 
-pub fn default_catalog() -> Catalog {
-    let mut c = Catalog {
-        list : Vec::new()
-    };
+impl fmt::Display for Id {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let Id(id) = *self;
+        write!(f, "{:X}", id)
+    }
+}
+
+pub fn add_default_data(data : &mut Data) {
+
+    let other = Section::new("Autre");
+    data.add_section(&other);
+    let greengrocer = Section::new("Primeur");
+    data.add_section(&greengrocer);
+    let frozen = Section::new("Surgelé");
+    data.add_section(&frozen);
+    let dairy_section = Section::new("Crémerie");
+    data.add_section(&dairy_section);
+    let organic = Section::new("Bio");
+    data.add_section(&organic);
+    let drink = Section::new("Boisson");
+    data.add_section(&drink);
+    let starchy = Section::new("Féculents");
+    data.add_section(&starchy);
+    let condiment = Section::new("Condiment");
+    data.add_section(&condiment);
+    let butcher = Section::new("Viande");
+    data.add_section(&butcher);
+    let fishshop = Section::new("Poisson");
+    data.add_section(&fishshop);
     
-    let p = Part {
-        id : 0,
-        name : "tomate".to_string(),
-        quantity : Quantity::Countable(1),
-        group : Group::Vegetable,
-        section : Section::Greengrocer,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let vegetable = Group::new("Légume");
+    data.add_group(&vegetable);
+    let fruit = Group::new("Fruit");
+    data.add_group(&fruit);
+    let protein = Group::new("Protéine");
+    data.add_group(&protein);
+    let carbohydrate = Group::new("Féculent");
+    data.add_group(&carbohydrate);
+    let fat = Group::new("Graisse");
+    data.add_group(&fat);
+    let dairy_group = Group::new("Laitage");
+    data.add_group(&dairy_group);
+    let cheese = Group::new("Fromage");
+    data.add_group(&cheese);
     
-    let p = Part {
-        id : 0,
-        name : "pomme".to_string(),
-        quantity : Quantity::Countable(1),
-        group : Group::Fruit,
-        section : Section::Greengrocer,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let tomate = Ingredient::new(
+        "tomate fraiche",
+        &vegetable,
+        &greengrocer,
+        &Quantity{val : 1.0, unit : Unit::Portion},
+    );
+    data.add_ingredient(&tomate);
     
-    let p = Part {
-        id : 0,
-        name : "steak haché".to_string(),
-        quantity : Quantity::Countable(1),
-        group : Group::Protein,
-        section : Section::Butcher,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let pomme = Ingredient::new(
+        "pomme",
+        &fruit,
+        &greengrocer,
+        &Quantity{val : 1.0, unit : Unit::Portion},
+    );
+    data.add_ingredient(&pomme);
+
+    let steak = Ingredient::new(
+        "steak haché",
+        &protein,
+        &butcher,
+        &Quantity{val : 1.0, unit : Unit::Portion},
+    );
+    data.add_ingredient(&steak);
     
-    let p = Part {
-        id : 0,
-        name : "tranche de pain".to_string(),
-        quantity : Quantity::Countable(2),
-        group : Group::Carbohydrate,
-        section : Section::Other,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let pain = Ingredient::new(
+        "tranche de pain",
+        &carbohydrate,
+        &other,
+        &Quantity{val : 1.0, unit : Unit::Portion},
+    );
+    data.add_ingredient(&pain);
     
-    let p = Part {
-        id : 0,
-        name : "huile".to_string(),
-        quantity : Quantity::Divisible(5,Unit::Centilitre),
-        group : Group::Fat,
-        section : Section::Other,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let huile = Ingredient::new(
+        "huile",
+        &fat,
+        &other,
+        &Quantity{val : 5.0, unit : Unit::Centilitre},
+    );
+    data.add_ingredient(&huile);
     
-    let p = Part {
-        id : 0,
-        name : "lait".to_string(),
-        quantity : Quantity::Divisible(5,Unit::Centilitre),
-        group : Group::Dairy,
-        section : Section::Dairy,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let lait = Ingredient::new(
+        "lait",
+        &dairy_group,
+        &dairy_section,
+        &Quantity{val : 1.0, unit : Unit::Portion},
+    );
+    data.add_ingredient(&lait);
     
-    let p = Part {
-        id : 0,
-        name : "emmental".to_string(),
-        quantity : Quantity::Divisible(30,Unit::Gram),
-        group : Group::Cheese,
-        section : Section::Dairy,
-    };
-    c.list.push(p);
-    c.list.last_mut().unwrap().id = c.list.len() as usize;
+    let emmental = Ingredient::new(
+        "emmental",
+        &cheese,
+        &dairy_section,
+        &Quantity{val : 30.0, unit : Unit::Gram},
+    );
+    data.add_ingredient(&emmental);
     
-    return c;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,167 @@
-extern crate gdk;
-extern crate gtk;
-use gtk::prelude::*;
+// extern crate gdk;
+// extern crate gtk;
+// use gtk::prelude::*;
 use std::rc::*;
 use std::cell::*;
 
 mod logic; use logic::*;
 
-fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-
-    let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    let catalog : Rc<RefCell<Catalog>> = Rc::new(RefCell::new(default_catalog()));
-    
-    window.set_title("Grand Bec Project");
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-    
-        
-    setup(window.clone(), catalog.clone());
-    
-    window.show_all();
-    gtk::main();
+fn main(){
+    let data : Rc<RefCell<Data>> = Rc::new(RefCell::new(Data::new()));
+    add_default_data(&mut data.borrow_mut());
 }
 
-fn create_combo_of_unit(unit : & Unit) -> gtk::ComboBoxText {
-    let c = gtk::ComboBoxText::new();
-    c.insert_text(Unit::Gram as i32, &Unit::Gram.to_string());
-    c.insert_text(Unit::Centilitre as i32, &Unit::Centilitre.to_string());
-    c.set_active(*unit as i32);
-    return c;
-}
-
-fn setup(window : gtk::Window, catalog : Rc<RefCell<Catalog>>) {
-
-    // division par deux panneaux
-    let paned = gtk::Paned::new(gtk::Orientation::Horizontal);
-    window.add(&paned);
-
-    // partie gauche : le planning
-    let left = gtk::ListBox::new();
-    paned.add1(&left);
-    left.set_size_request(300,300);
-    show_planning(&left, catalog.clone());
-
-    // partie droite : le catalogue d'ingrédients
-    let right = gtk::ListBox::new();
-    paned.add2(&right);
-    right.set_size_request(300,300);
-    show_catalogue(&right, catalog.clone());
-    
-}
-
-fn show_planning(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
-    let days = [
-        "lundi",
-        "mardi",
-        "mercredi",
-        "jeudi",
-        "vendredi",
-        "samedi",
-        "dimanche",
-    ];
-    let lunchs = [
-        "petit-déjeuner",
-        "déjeuner",
-        "gouter",
-        "diner",
-    ];
-    for day in days.iter() {
-        for lunch in lunchs.iter() {
-            show_empty_lunch(target, &format!("{} {}", day, lunch), catalog.clone());
-        }
-    }
-}
-
-fn show_empty_lunch(target : & gtk::ListBox, label : &str, catalog : Rc<RefCell<Catalog>>) {
-    let targets = vec![
-        gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
-    ];
-    
-    // test de drag&drop
-    let to = gtk::Label::new(label);
-    target.add(&to);
-    
-    to.drag_dest_set(gtk::DEST_DEFAULT_ALL, &targets, gdk::ACTION_COPY);
-    
-    let listbox = target.clone();
-    
-    to.connect_drag_data_received(move |w, _, _, _, s, _, _| {
-        let lbr : gtk::ListBoxRow = w
-            .get_parent()
-            .unwrap()
-            .downcast::<gtk::ListBoxRow>()
-            .unwrap();
-        let drop_pos = lbr.get_index();
-        let part_num : usize = s
-            .get_text().unwrap()
-            .parse().unwrap();
-        let part = & catalog.borrow().list[part_num];
-        let line = create_part(part);
-        listbox.insert(&line, drop_pos+1);
-        line.show_all();
-    });
-}
-
-fn create_part(part : & Part) -> gtk::EventBox {
-
-    let targets = vec![
-        gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
-    ];
-
-    let eventbox = gtk::EventBox::new();
-
-    let line = gtk::Grid::new();
-    eventbox.add(&line);
-    line.set_column_spacing(10);
-
-    let name = gtk::Label::new(&part.name as &str);
-    line.add(&name);
-    
-    eventbox.drag_source_set( gdk::MODIFIER_MASK, &targets, gdk::ACTION_COPY);
-    let data = part.id.to_string().clone();
-    eventbox.connect_drag_data_get(move |_, _, s, _, _| {
-        s.set_text(&data, data.len() as i32);
-    });
-    
-    match part.quantity {
-        Quantity::Countable(n) => {
-            let spin = gtk::SpinButton::new_with_range(
-                1.0, 1000.0, 25.0);
-            line.add(&spin);
-            spin.set_value(n as f64);
-        },
-        Quantity::Divisible(n, u) => {
-            let spin = gtk::SpinButton::new_with_range(
-                1.0, 50.0, 1.0);
-            line.add(&spin);
-            spin.set_value(n as f64);
-            line.add(&create_combo_of_unit(&u));
-        },
-    };
-    
-    line.add(&gtk::Label::new(&part.group.to_string() as &str));
-    
-    let section : String = format!("({})", part.section);
-    line.add(&gtk::Label::new(&section as &str));
-
-    return eventbox;
-}
-
-fn show_catalogue(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
-
-    for p in &catalog.borrow().list {
-        let line = create_part(p);
-        target.add(&line);
-    }
-}
+// fn main() {
+//     if gtk::init().is_err() {
+//         println!("Failed to initialize GTK.");
+//         return;
+//     }
+// 
+//     let window = gtk::Window::new(gtk::WindowType::Toplevel);
+//     
+//     window.set_title("Grand Bec Project");
+//     window.connect_delete_event(|_, _| {
+//         gtk::main_quit();
+//         Inhibit(false)
+//     });
+//     
+//         
+//     setup(window.clone(), catalog.clone());
+//     
+//     window.show_all();
+//     gtk::main();
+// }
+// 
+// fn create_combo_of_unit(unit : & Unit) -> gtk::ComboBoxText {
+//     let c = gtk::ComboBoxText::new();
+//     c.insert_text(Unit::Gram as i32, &Unit::Gram.to_string());
+//     c.insert_text(Unit::Centilitre as i32, &Unit::Centilitre.to_string());
+//     c.set_active(*unit as i32);
+//     return c;
+// }
+// 
+// fn setup(window : gtk::Window, catalog : Rc<RefCell<Catalog>>) {
+// 
+//     // division par deux panneaux
+//     let paned = gtk::Paned::new(gtk::Orientation::Horizontal);
+//     window.add(&paned);
+// 
+//     // partie gauche : le planning
+//     let left = gtk::ListBox::new();
+//     paned.add1(&left);
+//     left.set_size_request(300,300);
+//     show_planning(&left, catalog.clone());
+// 
+//     // partie droite : le catalogue d'ingrédients
+//     let right = gtk::ListBox::new();
+//     paned.add2(&right);
+//     right.set_size_request(300,300);
+//     show_catalogue(&right, catalog.clone());
+//     
+// }
+// 
+// fn show_planning(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
+//     let days = [
+//         "lundi",
+//         "mardi",
+//         "mercredi",
+//         "jeudi",
+//         "vendredi",
+//         "samedi",
+//         "dimanche",
+//     ];
+//     let lunchs = [
+//         "petit-déjeuner",
+//         "déjeuner",
+//         "gouter",
+//         "diner",
+//     ];
+//     for day in days.iter() {
+//         for lunch in lunchs.iter() {
+//             show_empty_lunch(target, &format!("{} {}", day, lunch), catalog.clone());
+//         }
+//     }
+// }
+// 
+// fn show_empty_lunch(target : & gtk::ListBox, label : &str, catalog : Rc<RefCell<Catalog>>) {
+//     let targets = vec![
+//         gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
+//     ];
+//     
+//     // test de drag&drop
+//     let to = gtk::Label::new(label);
+//     target.add(&to);
+//     
+//     to.drag_dest_set(gtk::DEST_DEFAULT_ALL, &targets, gdk::ACTION_COPY);
+//     
+//     let listbox = target.clone();
+//     
+//     to.connect_drag_data_received(move |w, _, _, _, s, _, _| {
+//         let lbr : gtk::ListBoxRow = w
+//             .get_parent()
+//             .unwrap()
+//             .downcast::<gtk::ListBoxRow>()
+//             .unwrap();
+//         let drop_pos = lbr.get_index();
+//         let part_num : usize = s
+//             .get_text().unwrap()
+//             .parse().unwrap();
+//         let part = & catalog.borrow().list[part_num];
+//         let line = create_part(part);
+//         listbox.insert(&line, drop_pos+1);
+//         line.show_all();
+//     });
+// }
+// 
+// fn create_part(part : & Part) -> gtk::EventBox {
+// 
+//     let targets = vec![
+//         gtk::TargetEntry::new("STRING", gtk::TARGET_SAME_APP, 0),
+//     ];
+// 
+//     let eventbox = gtk::EventBox::new();
+// 
+//     let line = gtk::Grid::new();
+//     eventbox.add(&line);
+//     line.set_column_spacing(10);
+// 
+//     let name = gtk::Label::new(&part.name as &str);
+//     line.add(&name);
+//     
+//     eventbox.drag_source_set( gdk::MODIFIER_MASK, &targets, gdk::ACTION_COPY);
+//     let data = part.id.to_string().clone();
+//     eventbox.connect_drag_data_get(move |_, _, s, _, _| {
+//         s.set_text(&data, data.len() as i32);
+//     });
+//     
+//     match part.quantity {
+//         Quantity::Countable(n) => {
+//             let spin = gtk::SpinButton::new_with_range(
+//                 1.0, 1000.0, 25.0);
+//             line.add(&spin);
+//             spin.set_value(n as f64);
+//         },
+//         Quantity::Divisible(n, u) => {
+//             let spin = gtk::SpinButton::new_with_range(
+//                 1.0, 50.0, 1.0);
+//             line.add(&spin);
+//             spin.set_value(n as f64);
+//             line.add(&create_combo_of_unit(&u));
+//         },
+//     };
+//     
+//     line.add(&gtk::Label::new(&part.group.to_string() as &str));
+//     
+//     let section : String = format!("({})", part.section);
+//     line.add(&gtk::Label::new(&section as &str));
+// 
+//     return eventbox;
+// }
+// 
+// fn show_catalogue(target : &gtk::ListBox, catalog : Rc<RefCell<Catalog>>) {
+// 
+//     for p in &catalog.borrow().list {
+//         let line = create_part(p);
+//         target.add(&line);
+//     }
+// }
